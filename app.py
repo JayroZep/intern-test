9f7b92dde79a434e06946e35ed32a2ed90e35c2e
JayroZep <pmolikyeah@gmail.com>
2015-12-28 13:43:06 +0100

    Flash error fixed.

--- app.py
+++ app.py
@@ -6,15 +6,14 @@ from flask import Flask, flash, request, render_template
 app = Flask(__name__)
 app.secret_key = 'life is pointless'
 
-with open('products.yml') as _f: #the content of products.yml is copied into vat PRODUCTS
+with open('products.yml') as _f: #the content of products.yml is copied into var PRODUCTS
     PRODUCTS = yaml.load(_f)
 
-with open('denominations.yml') as _f: #the content of denominations.yml is copied into vat DENOMINATIONS
+with open('denominations.yml') as _f: #the content of denominations.yml is copied into var DENOMINATIONS
     DENOMINATIONS = yaml.load(_f)
 
 
 ORDER_DB = 'orders.yml' #void at the beginning
-
 
 def record_order(product_id):
     order_id = str(uuid.uuid4()).split('-', 1)[0]
@@ -28,9 +27,12 @@ def record_order(product_id):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    context = {}
     if request.method == 'POST':
-        flash('Order Placed Successfully') #the flash won't work - 'connection reset - 200'
-    return render_template('index.jinja', products=PRODUCTS, title='Order Form')
+        record_order(0) #0 just for testing
+        print(request.form)
+        flash('Order Placed Successfully')
+    return render_template('index.jinja', products=PRODUCTS, title='Order Form', **context)
 
 @app.route('/404')
 def error_page():
@@ -45,7 +47,10 @@ def confirmation(order_id):
     if order is None:
         return render_template('404')
     # TODO other stuff has to be calculated here.
-    return render_template('confirmation.jinja', order_id=order_id, title='Order Confirmation')
+    # We have to set all variables that will be displayed in confirmation.jinja : order_id, amount_paid, item_price, change_due
+    # But how to access those variables, that are created directly in the .jinja ?
+    # How can I use the result of the POST method in index() ? Should I create some temp file and put data from the form into?
+    return render_template('confirmation.jinja', order_id=order_id , title='Order Confirmation')
 
 
 if __name__ == '__main__':
